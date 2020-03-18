02c7017c2afe6236bb9b4e4aadede975a805bd86
Jkeyuk <Jonkero2@gmail.com>
2020-03-18 10:26:08 -0400

    support 18841 create project structure
    https://redmine.biodiversity.agr.gc.ca/issues/18841
    
    Completley remove agent module configuration.
    Not needed yet.

--- src/main/java/ca/gc/aafc/agent/api/AgentModuleApiLauncher.java
+++ src/main/java/ca/gc/aafc/agent/api/AgentModuleApiLauncher.java
@@ -2,7 +2,6 @@ package ca.gc.aafc.agent.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 /**
  * Launches the application.
@@ -10,7 +9,6 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 // CHECKSTYLE:OFF HideUtilityClassConstructor (Configuration class can not have
 // invisible constructor, ignore the check style error for this case)
 @SpringBootApplication
-@EnableConfigurationProperties(AgentModuleConfiguration.class)
 public class AgentModuleApiLauncher {
   public static void main(String[] args) {
     SpringApplication.run(AgentModuleApiLauncher.class, args);
