8c5c5478a4a879ac8249596bb4289963052aaa75
Jkeyuk <Jonkero2@gmail.com>
2020-03-18 10:12:35 -0400

    support 18841 create project structure
    https://redmine.biodiversity.agr.gc.ca/issues/18841
    
    - Remove unused configuration variables from AgentModuleConfiguration and yml

--- src/main/java/ca/gc/aafc/agent/api/AgentModuleConfiguration.java
+++ src/main/java/ca/gc/aafc/agent/api/AgentModuleConfiguration.java
@@ -7,26 +7,6 @@ import org.springframework.boot.context.properties.ConstructorBinding;
 @ConfigurationProperties(prefix = "agent-module")
 public class AgentModuleConfiguration {
 
-  private String defaultLicenceURL;
-  private String defaultCopyright;
-  private String defaultCopyrightOwner;
-
-  public AgentModuleConfiguration(String defaultLicenceURL, String defaultCopyright, String defaultCopyrightOwner) {
-    this.defaultLicenceURL = defaultLicenceURL;
-    this.defaultCopyright = defaultCopyright;
-    this.defaultCopyrightOwner = defaultCopyrightOwner;
-  }
-
-  public String getDefaultLicenceURL() {
-    return defaultLicenceURL;
-  }
-
-  public String getDefaultCopyright() {
-    return defaultCopyright;
-  }
-
-  public String getDefaultCopyrightOwner() {
-    return defaultCopyrightOwner;
-  }
+  public AgentModuleConfiguration() {}
 
 }
